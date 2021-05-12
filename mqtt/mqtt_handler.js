4449a2a93ee5cf40429f76bb4c9db90c06af8c9e
gambl3r08 <robertojoselozada@gmail.com>
2021-05-12 18:16:24 -0500

    pre doc

--- mqtt/mqtt_handler.js
+++ mqtt/mqtt_handler.js
@@ -5,7 +5,8 @@ class MqttHandler{
         this.mqttClient = null
         //Lya mqtt server mqtt://mqtt.lyaelectronic.com:1883
         // Mosquitto mqtt broker para pruebas mqtt:localhost
-        this.host = 'mqtt://mqtt.lyaelectronic.com:1883' 
+        //Broker publico  mqtt:broker.hivemq.com
+        this.host = 'mqtt:broker.hivemq.com' 
         this.username = 'test'
         this.password = 'test'
     }
@@ -17,24 +18,26 @@ class MqttHandler{
             this.mqttClient.end()
         })
 
-        this.mqttClient.on('connect', ()=>{
+        this.mqttClient.on('connect', (err)=>{
+            if(err) console.log(err.message)
             console.log('mqtt client connected')
         })
 
-        this.mqttClient.subscribe('lyatest', {qos: 0})
+        this.mqttClient.subscribe('lyatest/', {qos: 0})
 
         this.mqttClient.on('message', (topic, message)=>{
             console.log(message.toString())
         })
 
-        this.mqttClient.on('close', ()=>{
+        this.mqttClient.on('close', (err)=>{
+            if(err) console.log(err.message)
             console.log('mqtt client disconnected')
         })
     }
 
     sendMessage(id, message){
         const out = `Message:  ${message}, user_id:${id}`
-        this.mqttClient.publish('lyatest', out)
+        this.mqttClient.publish('lyatest/', out)
     }
 }
 
